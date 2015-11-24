903deb30a0e6ea161043a5e2a48d520146fba646
krikmo <616c6c@gmail.com>
2015-11-23 23:33:18 -0200

    Make heuristic option to choose password length
    Also, update README accordingly

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Simple things for simple tasks.
 
-* [**"Random" Password Generator**](https://krikmo.github.io/pass.html) is exactly what you think it is. The special thing is that the passwords are 23 characters long, alphanumeric and (maybe) with a few special characters. Nice.
+* [**"Random" Password Generator**](https://krikmo.github.io/pass.html) is exactly what you think it is. The special thing is that the passwords are alphanumeric and (maybe) with a few special characters. You can choose its length, but bear with me.
 * [**The Coin Flipper**](https://krikmo.github.io/flip.html) is *also* exactly what you think it is.
 * [**Time**](https://krikmo.github.io/time.html) is... wait for it... exactly what you think it is.
 
--- src/gen.js
+++ src/gen.js
@@ -1,11 +1,21 @@
+function getUrlData() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+var text = "";
+var getLength = getUrlData()["len"];
 function makepass() {
-    var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@";
-
-    for (var i = 0; i < 23; i++)
+    for (var i = 0; i < getLength; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
-
     return text;
-};
-
+}
 document.getElementById("num").innerHTML = makepass();
+
+if (text === "") {
+    document.getElementById("num").innerHTML = "To generate your password, you need to format the URL like this: \"pass.html?len=23\", where 23 is the length of your password.";
+} else {
+}
