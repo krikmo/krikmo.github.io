7747a5a0275844916c6e19118bee2cfcacd9d514
Krik Mohammed <krikmo@users.noreply.github.com>
2023-09-23 14:08:23 -0300

    Update mover.js to catch live streams
    Not perfect yet

--- mover.js
+++ mover.js
@@ -1,39 +1,49 @@
-javascript:(function(){ 
-    if (location.href.includes('youtube.com/watch?v=')) {
-        var durationElement = document.querySelector('meta[itemprop=duration]');
-        
-        if (durationElement) {
-            var duration = durationElement.getAttribute('content');
-            var hoursMatch = duration.match(/(\d+)H/);
-            var minutesMatch = duration.match(/(\d+)M/);
-            var secondsMatch = duration.match(/(\d+)S/);
-
-            var hours = 0;
-            var minutes = 0;
-            var seconds = 0;
-
-            if (hoursMatch) hours = parseInt(hoursMatch[1], 10);
-            if (minutesMatch) minutes = parseInt(minutesMatch[1], 10);
-            if (secondsMatch) seconds = parseInt(secondsMatch[1], 10);
-
-            hours += Math.floor(minutes / 60);
-            minutes = minutes % 60;
-
-            var formattedDuration;
-
-            if (hours > 0) {
-                formattedDuration = hours.toString().padStart(2, '0') + ':' +
-                                    minutes.toString().padStart(2, '0') + ':' +
-                                    seconds.toString().padStart(2, '0');
-            } else {
-                formattedDuration = minutes.toString().padStart(2, '0') + ':' +
-                                    seconds.toString().padStart(2, '0');
+javascript:(function() {
+    let retryCount = 0;
+    function mainLogic() {
+        if (location.href.includes('youtube.com/watch?v=')) {
+            const durationElement = document.querySelector('meta[itemprop=duration]');
+            const liveElement = document.body.textContent.includes('Live');
+            if (durationElement) {
+                processDuration(durationElement);
+                location='https://krikmo.github.io/black.html#url:'+encodeURIComponent(location.href)+'?|title:'+encodeURIComponent(document.title);
+            } else if (liveElement) {
+                document.title = `LIVE | ${document.title}`;
+                location='https://krikmo.github.io/black.html#url:'+encodeURIComponent(location.href)+'?|title:'+encodeURIComponent(document.title);
+            } else if (retryCount < 5) {
+                retryCount++;
+                setTimeout(mainLogic, 1000);
             }
-
-            if (formattedDuration !== '00:00') {
-                document.title = formattedDuration + ' | ' + document.title; 
-            }
+        } else {
+            location='https://krikmo.github.io/black.html#url:'+encodeURIComponent(location.href)+'?|title:'+encodeURIComponent(document.title);
         }
     }
-    location='https://krikmo.github.io/black.html#url:'+encodeURIComponent(location.href)+'?|title:'+encodeURIComponent(document.title)    ;
+
+    function processDuration(durationElement) {
+        const duration = durationElement.getAttribute('content');
+        let hours = 0, minutes = 0, seconds = 0;
+        const hoursMatch = duration.match(/(\d+)H/);
+        const minutesMatch = duration.match(/(\d+)M/);
+        const secondsMatch = duration.match(/(\d+)S/);
+
+        if (hoursMatch) hours = parseInt(hoursMatch[1], 10);
+        if (minutesMatch) minutes = parseInt(minutesMatch[1], 10);
+        if (secondsMatch) seconds = parseInt(secondsMatch[1], 10);
+
+        hours += Math.floor(minutes / 60);
+        minutes = minutes % 60;
+
+        let formattedDuration;
+        if (hours > 0) {
+            formattedDuration = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        } else {
+            formattedDuration = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        }
+
+        if (formattedDuration !== '00:00') {
+            document.title = `${formattedDuration} | ${document.title}`;
+        }
+    }
+
+    mainLogic();
 })();
